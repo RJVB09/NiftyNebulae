cb8ae22f2206e18ec85c1e6518293a75f0748f13
RJVB09 <49564898+RJVB09@users.noreply.github.com>
2022-12-08 15:23:33 +0100

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NiftyNebulae
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class Main : MonoBehaviour
+    {
+        void Update()
+        {
+            Debug.Log("ALLAH OBAMA PLUGIN WORKING LMAO");
+        }
+    }
+}
